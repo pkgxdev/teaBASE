97fad239f8f6aa745f32c23677c489e5c3e0e14b
Max Howell <mxcl@me.com>
2024-10-07 09:18:54 -0400

    Initial Commit

---
+++ Custodian/Custodian.h
@@ -0,0 +1,14 @@
+//
+//  Custodian.h
+//  Custodian
+//
+//  Created by Maxim Howell on 10/7/24.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Custodian : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Custodian/Custodian.m
@@ -0,0 +1,16 @@
+//
+//  Custodian.m
+//  Custodian
+//
+//  Created by Maxim Howell on 10/7/24.
+//
+
+#import "Custodian.h"
+
+@implementation Custodian
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
