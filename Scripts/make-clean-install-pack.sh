c9fb958fcd5814ea9a94c2c537cac29cb077cec4
Max Howell <mxcl@me.com>
2024-12-19 06:54:31 -0500

    wip

--- Scripts/make-clean-install-pack.sh
+++ Scripts/make-clean-install-pack.sh
@@ -57,7 +57,7 @@ add_file() {
   gitdirs=()
   mapfile -d '' gitdirs < <(find "$1" -name .git -type d -print0)
 
-  echo "foo: $1 ${gitdirs[@]}"
+  echo "foo: $1 ${gitdirs[@]} ${#gitdirs[@]}"
 
   if [ "${#gitdirs[@]}" -eq 0 ]; then
     tar rf "$d/dotfiles.tar" "$1"
